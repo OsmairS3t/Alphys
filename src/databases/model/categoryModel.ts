481b0773c1f6413d8bf46da57577926c06b3979f
Osmair <osmair144@gmail.com>
2024-04-16 17:51:50 -0300

    Updated db

--- src/databases/model/categoryModel.ts
+++ src/databases/model/categoryModel.ts
@@ -1,12 +1,9 @@
 import { Model } from "@nozbe/watermelondb";
-import { field, text } from "@nozbe/watermelondb/decorators";
+import { text, field } from "@nozbe/watermelondb/decorators";
 
 export class CategoryModel extends Model {
   static table = 'categories'
-
   @field('name') 
-  name !: string;
-  
- 
+  name!:string
 }
 
